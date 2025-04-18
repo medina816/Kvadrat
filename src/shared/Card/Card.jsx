e2a7c93fe802b4853e89abfb67da01cdb6d0d825
elmir55 <elmira.050405@gmail.com>
2025-04-18 19:24:59 +0600

    fis: Cards and Advantages component

--- src/shared/Card/Card.jsx
+++ src/shared/Card/Card.jsx
@@ -3,47 +3,59 @@ import { FaChevronDown } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import cardData from './card';
 import PropertyCard from './PropertyCard';
-import '../../app/index.css';
-import Container from '../../components/Container/Container'
+import '../../app/index.css'; 
 
 function Card() {
-  return ( 
-    <Container > 
+  return (
     <div className="w-full bg-black py-6 flex justify-center">
       <div className="w-full bg-black max-w-[1293px]">
-        <div className="flex items-center justify-between gap-6 mb-8 px-4 lg:px-0 flex-wrap lg:flex-nowrap">
-          <button className="w-[283px] bg-[#C8180C] text-white px-6 h-[46px] rounded-full flex items-center justify-between text-base">
-            Категории <FaChevronDown className="ml-3" />
-          </button>
 
-          <p className="w-full font-semibold text-base sm:text-lg lg:text-xl leading-6 sm:leading-7 lg:leading-7 text-white uppercase  mt-0 lg:mt-[-10px] lg:w-[661px] text-center lg:text-left">
-            Выбирайте квартиру для жизни или инвестиций. Предложение доступно
-            <span className="bg-[#DC2215] ml-2 cursor-pointer text-white px-4 sm:px-6 lg:px-9  inline-block mt-1 lg:mt-0">
-              К ПОКУПКЕ ПРЯМО СЕЙЧАС
-            </span>
-          </p>
+      <div className="flex items-center  justify-between gap-6 mb-8 px-4 lg:px-0 flex-wrap lg:flex-nowrap">
+      <button className="bg-[#C8180C] lg:ml-1 text-white px-5 h-[35px] lg:h-[46px] rounded-full flex items-center justify-between  font-medium lg:text-lg text-[12px] lg:w-[283px] lg:mr-0 ">
+  Категории <FaChevronDown className="ml-7 lg:ml-[25px] " />
+</button>
 
-          <Link to="/properties">
-            <h2 className="text-white underline text-xl cursor-pointer  sm:mt-4 mr-1">
-              Смотреть больше
-            </h2>
-          </Link>
-        </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 gap-y-12 px-4 lg:px-0">
+<p className="w-full sm:w-[268px] lg:w-[661px] font-medium  text-[13px] lg:text-xl   lg:leading-7 text-white uppercase mt-0 lg:mt-[-10px] text-left lg:text-left lg:mr-0 mr-[30px]">
+  Выбирайте квартиру для жизни или инвестиций. Предложение доступно 
+  <span className="bg-[#DC2215]  rounded-[1px] cursor-pointer text-white  sm:px-6 lg:mr-0 mr-[60px] lg:px-9   lg:ml-2 inline-block mt-1 lg:mt-0">
+    К ПОКУПКЕ ПРЯМО СЕЙЧАС
+  </span>
+</p>
+  <Link to="/properties">
+  <h2 className="text-white font-normal border-b border-white w-fit text-[13px] lg:text-xl cursor-pointer sm:mt-4 mr-1 ">
+  Смотреть больше
+</h2>
+  </Link>
+</div>
+        <div className="hidden lg:grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-y-12 sm:gap-y-8 ">
           {cardData.slice(0, 8).map((item) => (
             <PropertyCard key={item.id} item={item} />
           ))}
         </div>
 
-        <div className="w-full flex justify-center mt-20 px-4">
-          <button className="bg-[#FF3729] w-[250px] h-[57px] text-xl text-white rounded-full hover:bg-red-600 transition ">
-            Смотреть все
-          </button>
+        <div className="lg:hidden overflow-x-auto px-4 hide-scrollbar">
+  <div className="flex gap-3 w-max  ">
+    {Array(4).fill(0).map((_, i) => (
+      <div key={i} className="flex flex-col flex-none" style={{ width: '151px' }}>
+        <div className="mb-[45px]">
+          <PropertyCard item={cardData[i]} small />
+        </div>
+        <div>
+          <PropertyCard item={cardData[i + 4]} small />
         </div>
       </div>
-    </div> 
-    </Container>
+    ))}
+  </div>
+</div>
+<div className="w-full justify-center mt-20 px-4 hidden lg:flex">
+  <button className="bg-[#FF3729] w-[250px] h-[57px] text-xl text-white rounded-full hover:bg-red-600 transition">
+    Смотреть все
+  </button>
+</div>
+
+      </div>
+    </div>
   );
 }
 
--- src/shared/Card/PropertyCard.jsx
+++ src/shared/Card/PropertyCard.jsx
@@ -10,50 +10,79 @@ function PropertyCard({ item }) {
 
   const prevImage = () => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + item.images.length) % item.images.length);
-  }; 
+  };
 
   return (
-    <div className="bg-white rounded-[4px] shadow-md hover:shadow-xl transition duration-300 overflow-hidden flex flex-col pb-6 h-[530px]">
+    <div className="
+      bg-white rounded-[4px] shadow-md hover:shadow-xl 
+      transition duration-300 overflow-hidden flex flex-col
+      pb-2 lg:pb-6 lg:w-[310px]
+      sm:w-[151px] sm:h-[227px] lg:h-[464px] 
+    ">
       <div className="relative">
-        <img src={item.images[currentIndex]} alt={item.title} className="w-full h-[277px] object-cover" />
-        
-        <button onClick={prevImage} className="absolute top-1/2 left-2 -translate-y-1/2 text-white">
-          <IoIosArrowBack className="size-6 ml-1" />
+        <img
+          src={item.images[currentIndex]}
+          alt={item.title}
+          className="w-full lg:h-[207px] sm:h-[101px] object-cover"
+        />
+
+        <button
+          onClick={prevImage}
+          className="absolute top-1/2 left-1 sm:left-2 -translate-y-1/2 text-white"
+        >
+          <IoIosArrowBack className="size-4 sm:size-6 ml-0.5 sm:ml-1" />
         </button>
 
-        <button onClick={nextImage} className="absolute top-1/2 right-2 -translate-y-1/2 text-white">
-          <IoIosArrowForward className="size-6 mr-1" />
+        <button
+          onClick={nextImage}
+          className="absolute top-1/2 right-1 sm:right-2 -translate-y-1/2 text-white"
+        >
+          <IoIosArrowForward className="size-4 sm:size-6 mr-0.5 sm:mr-1" />
         </button>
 
-        <div className="absolute bottom-2 left-1/2 -translate-x-1/2 flex gap-1">
+        <div className="absolute bottom-1 sm:bottom-2 left-1/2 -translate-x-1/2 flex gap-0.5 sm:gap-1">
           {item.images.map((_, i) => (
-            <div key={i} className={`w-2.5 h-2.5 rounded-full ${i === currentIndex ? 'bg-red-600' : 'bg-gray-300'}`} />
+            <div
+              key={i}
+              className={`w-1.5 h-1.5 sm:w-2.5 sm:h-2.5 rounded-full ${
+                i === currentIndex ? 'bg-red-600' : 'bg-gray-300'
+              }`}
+            />
           ))}
         </div>
       </div>
 
-      <div className="px-4 mt-4 space-y-4 ">
-        <h3 className="text-lg font-semibold text-[#0A0200] leading-tight">
-          <span className="block">3 - комнатная квартира на</span>
-          <span className="block text-center">улице Киевская 30</span>
-        </h3>
-
-        <div className="flex flex-col sm:flex-row justify-between mt-3 ml-2 sm:ml-4">
-          <p className="text-sm sm:text-base font-normal text-[#0A0200] text-left sm:text-start">
+      <div className="px-2 sm:px-4 mt-2 sm:mt-4 space-y-1 lg:space-y-4">
+      <h3 className="text-center font-medium text-[#0A0200] leading-tight text-[10px] sm:text-[24px] lg:text-lg">
+  <span className="block leading-tight">3 - комнатная квартира на</span>
+  <span className="block leading-tight">улице Киевская 30</span>
+</h3>
+        <div className="flex justify-center mt-1 sm:mt-4">
+          <p className="text-[8px] mr-[39px]  sm:text-[10px] lg:text-base font-normal text-[#0A0200] lg:mr-[70px]">
             Площадь: {item.area}
           </p>
         </div>
 
-        <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mt-5 gap-2 sm:gap-0">
-          <p className="text-base sm:text-lg font-semibold text-[#0A0200]">Цена</p>
-          <div className="flex items-center">
-            <span className="text-base sm:text-lg font-semibold text-[#0A0200]">{item.price}</span>
-          </div>
-        </div>
+        <div className="flex items-center justify-between px-2 sm:px-4 lg:px-4 lg:justify-between lg:mr-[5px] lg:mt-2">
+  <p className="text-[8px] mt-2 sm:text-[10px] lg:text-lg font-semibold text-[#0A0200]"> 
+    Цена
+  </p>
+  <span className="text-[8px] mt-2 sm:text-[10px] lg:text-lg font-semibold text-[#0A0200]">
+    {item.price}
+  </span>
+</div>
 
-        <button className="w-full bg-[#DC2215] text-white text-lg rounded-full h-[50px] hover:bg-red-700 transition">
-          Подробнее
-        </button>
+
+        <div>
+          <button className="
+            w-full bg-[#DC2215] mt-1 mb-1 text-white rounded-full  
+            text-[8px] lg:w-[261px] sm:w-[127px] sm:text-sm lg:text-lg 
+            h-[22px] sm:h-[40px] lg:h-[50px] 
+            hover:bg-red-700 transition lg:mt-[12px] lg:ml-[5px] 
+          ">
+            Подробнее
+          </button>
+        </div>
       </div>
     </div>
   );
