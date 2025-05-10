0f8b4f94d7bd34da895fdffd5fee1256f02f3637
Tologon uulu Saridin <saridintologonuulu@gmail.com>
2025-05-10 11:05:12 +0600

    AllPropertiesApi.js AllProperties

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,14 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { bannerApi } from '../services/bannerApi';
 import { productsApi } from '../services/productsApi';
+import { allPropertiesApi } from '../services/AllPropertiesApi';
 
 const store = configureStore({
   reducer: {
     [bannerApi.reducerPath]: bannerApi.reducer,
     [productsApi.reducerPath]: productsApi.reducer,
+    [allPropertiesApi.reducerPath]: allPropertiesApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(bannerApi.middleware, productsApi.middleware),
+    getDefaultMiddleware().concat(bannerApi.middleware, productsApi.middleware, allPropertiesApi.middleware),
 });
 
 export default store;
